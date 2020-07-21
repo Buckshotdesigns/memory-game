6bb6b69f123a413f84b52fa45f5a7737012efbb9
bryanbuckus <bryanbuckus@gmail.com>
2020-07-21 12:42:18 -0400

    second commit

---
+++ assets/scripts/game.js
@@ -0,0 +1,33 @@
+DOM.addEventListener("DomContentLoaded", () => {
+
+const cardArray = [
+{
+    name: "animals",
+    img: "../images/animals.jpg"
+},
+{
+    name: "chimp",
+    img: "../images/chimp.jpg" 
+},
+{
+    name: "gato",
+    img: "../images/gator.jpg" 
+},
+{
+    name: "giraffe",
+    img: "../images/giraffe.jpg" 
+},
+{
+    name: "rhino",
+    img: "../images/rhino.jpg" 
+},
+{
+    name: "tiger",
+    img: "../images/tiger.jpg" 
+},
+
+];
+
+
+
+})
